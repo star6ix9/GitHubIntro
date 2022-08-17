350e16b9c8f5cb878a42fdd42a9cd56e14166adb
Ian <mriancuatico@gmail.com>
2022-08-17 07:21:40 +0000

    I am commiting

---
+++ example.py
@@ -0,0 +1,14 @@
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 350 bytes | 350.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/star6ix9/GitHubIntro.git
+   f1b762d..fd24e75  main -> main
+student-04-db49e0e7c878@linux-instance:~/GitHubIntro$ nano example.py
+student-04-db49e0e7c878@linux-instance:~/GitHubIntro$ git add example.py
+student-04-db49e0e7c878@linux-instance:~/GitHubIntro$ git commit
+Aborting commit due to empty commit message.
+student-04-db49e0e7c878@linux-instance:~/GitHubIntro$ nano example.py
+  GNU nano 5.4                                                    example.py *
+def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation(
